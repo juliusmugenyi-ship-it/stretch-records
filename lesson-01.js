042867e7be4c6706ab686fed29d8d4b53bde56c5
Julius Mugenyi <julius.mugenyi@gmail.com>
2026-08-04 23:53:27 +0200

    Demonstrate JSON stringify parse round trip

--- lesson-01.js
+++ lesson-01.js
@@ -18,3 +18,19 @@
 
 // JSON error observed:
 // Uncaught SyntaxError: Unexpected token ']', ... is not valid JSON (On Bob marley)
+
+// JSON round trip practice
+
+const artist = {
+  name: "Bob Marley",
+  genre: "Reggae",
+  songs: ["One Love", "Three Little Birds"],
+};
+
+// Convert object to JSON text
+const artistText = JSON.stringify(artist);
+console.log(artistText);
+
+// Convert JSON text back into an object
+const parsedArtist = JSON.parse(artistText);
+console.log(parsedArtist.name);
