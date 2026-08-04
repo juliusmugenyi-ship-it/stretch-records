1fe2deb47e51112c12f8805e246f27094522456c
Julius Mugenyi <julius.mugenyi@gmail.com>
2026-08-04 23:26:22 +0200

    Document JSON parsing error

--- lesson-01.js
+++ lesson-01.js
@@ -15,3 +15,6 @@
 // This separation is the point because data can be updated independently
 // from the code that renders it and the page structure.
 // while the rendering logic and page structure remain reusable.
+
+// JSON error observed:
+// Uncaught SyntaxError: Unexpected token ']', ... is not valid JSON (On Bob marley)
